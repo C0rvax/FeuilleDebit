400a282f71f076e28234ec0d07df882a4e4ed9c2
Corvax <arthurduvillaret@gmail.com>
2023-05-23 00:59:10 +0200

    V0.2

--- calculDebit.py
+++ calculDebit.py
@@ -1,6 +1,5 @@
 
-
-import math
+# DEFINITION DES FONCTIONS
 
 
 def writeAll(nombrePx, longPx, largPx, ref):
@@ -13,65 +12,61 @@ def writeAll(nombrePx, longPx, largPx, ref):
         fc.close
 
 
-EpaisseurFond = 0
-ratio = 100
-hauteurSocle = 0
-EpaisseurPorte = 0
-
+# INPUT
 nomProjet = input("Nom du projet :\n")
 dimensions = input(
     "Entrer les dimensions du meuble sous le format : hauteur;Largeur;Profondeur (en mm) :\n")
 li = list(dimensions.split(";"))
-hauteurTotale = int(li[0])
-LargeurTotale = int(li[1])
-profondeurTotale = int(li[2])
-
 epaisseurMontant = int(input("Entrer l'épaisseur des montants :\n"))
 epaisseurPetit = int(input("Entrer l'épaisseur des étagères :\n"))
 hauteurSocle = int(input("Entrer la hauteur du socle (0 si pas de socle):\n"))
 epaisseurFond = int(input("Entrer Epaisseur du fond (0 si pas de fond):\n"))
-Recouvrement = int(input(
+recouvrement = int(input(
     "Taper '1' si portes et socle en recouvrement, sinon taper '0' :\n"))
-NbCaisson = int(input("Entrer le nombre de caisson :\n"))
+nbCaisson = int(input("Entrer le nombre de caisson :\n"))
 
-if Recouvrement != 0:
+# VARIABLES
+hauteurTotale = int(li[0])
+largeurTotale = int(li[1])
+profondeurTotale = int(li[2])
+if recouvrement != 0:
     profMontant = profondeurTotale - epaisseurPetit
 else:
     profMontant = profondeurTotale
-profondeurInt = int((profondeurTotale - epaisseurFond - EpaisseurPorte))
+profondeurInt = int((profondeurTotale - epaisseurFond -
+                    recouvrement * epaisseurPetit))
 hauteurInterieur = (hauteurTotale - hauteurSocle - epaisseurPetit)
 hauteurMontantInter = (hauteurTotale - epaisseurPetit)
-LargeurInt = (LargeurTotale - 2 * epaisseurMontant)
-nbMontant = NbCaisson - 1
+largeurInt = (largeurTotale - 2 * epaisseurMontant)
+nbMontant = nbCaisson - 1
 
-
-with open(nomProjet + '.txt', 'a') as f:
+# CREATION FICHIERS TXT
+with open(nomProjet + '.txt', 'w') as f:
     f.write("En " + str(epaisseurMontant) + "mm d'épaisseur\n")
     f.close
 
-with open(nomProjet + 'detail.txt', 'a') as fc:
+with open(nomProjet + 'detail.txt', 'w') as fc:
     fc.write(nomProjet + "\nDimensions : " + str(dimensions) + "mm")
     fc.write("Epaisseur Montant : " + str(epaisseurMontant) +
              " mm ; Etagères : " + str(epaisseurPetit) + "mm")
     fc.write("Hauteur du socle : " + str(hauteurSocle) + " mm\nEpaisseur du fond : " +
-             str(epaisseurFond) + " mm\nRecouvrement : " + str(Recouvrement) + "\n\n")
+             str(epaisseurFond) + " mm\nRecouvrement : " + str(recouvrement) + "\n\n")
     fc.close
 
 writeAll(str(2), str(hauteurTotale), str(profMontant), "Montants Exterieurs")
 
-if NbCaisson > 1:
+if nbCaisson > 1:
     writeAll(str(nbMontant), str(hauteurMontantInter),
              str(profondeurInt), "Montants Interieurs")
 
 if epaisseurPetit < epaisseurMontant:
     with open(nomProjet + '.txt', 'a') as f:
-        f.write("\n")
-        f.write("En " + str(epaisseurPetit) + "mm d'épaisseur\n")
+        f.write("\nEn " + str(epaisseurPetit) + "mm d'épaisseur\n")
         f.close
 
-writeAll(str(1), str(LargeurTotale), str(profMontant), "Corniche")
+writeAll(str(1), str(largeurTotale), str(profMontant), "Corniche")
 
-for i in range(NbCaisson):
+for i in range(nbCaisson):
     largeurcaisson = int(input("Caisson " + str(i+1) + " - Entrer la largeur (" +
                          str(LargeurInt) + " mm disponible) :\n"))  # reponse < Largeur int
     porte = int(input("Caisson " + str(i) +
@@ -83,12 +78,12 @@ for i in range(NbCaisson):
                  " mm\nNombre de porte : " + str(porte) + "\nNombre d'étagères : " + str(nbEtageres) + "\n")
         fc.close
 
-    if Recouvrement != 0:
+    if recouvrement != 0:
         hporte = hauteurTotale - hauteurSocle - 4
         lporte = int((largeurcaisson / porte) - (porte * 4))
         writeAll(str(porte), str(hporte), str(lporte), "Portes")
         if hauteurSocle != 0 and i < 2:
-            writeAll(str(1), str((LargeurTotale)),
+            writeAll(str(1), str((largeurTotale)),
                      str(hauteurSocle), "Bandeau Socle")
     else:
         hporte = hauteurInterieur - 4
