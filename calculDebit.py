c21746951e726506c5539761a89ca61b76976787
Corvax <arthurduvillaret@gmail.com>
2023-05-23 12:38:12 +0200

    Ajout des tests d'input

--- calculDebit.py
+++ calculDebit.py
@@ -1,35 +1,52 @@
+# CONSTANTES
+cheminDossier = r'D:\\Code\DATA'
+hauteurPxMax = 2500
+largeurPxMax = 1220
+epaisseurMin = 10
+epaisseurMax = 100
+largeurMeubleMax = 100000
 
 # DEFINITION DES FONCTIONS
 
 
 def writeAll(nombrePx, longPx, largPx, ref):
-    with open(nomProjet + '.txt', 'a') as f:
+    with open(fichierFournisseur, 'a') as f:
         f.write("-" + nombrePx + " fois " + longPx + " x " + largPx + "mm\n")
         f.close
-    with open(nomProjet + 'detail.txt', 'a') as fc:
+    with open(fichierDetail, 'a') as fc:
         fc.write("-" + nombrePx + " fois " + longPx +
                  " x " + largPx + "mm - " + ref + "\n")
         fc.close
 
 
+def verif(phrase, min, max):
+    entree = input(phrase + " =\n")
+    while not entree.isdigit() or not min <= int(entree) <= max:
+        entree = input(phrase + " MIN " + str(min) +
+                       " ; MAX " + str(max) + " =\n")
+        # while not min <= int(entree) <= max:
+        #    entree = input(phrase + " :\n")
+    return int(entree)
+
+
 # INPUT
-nomProjet = input("Nom du projet :\n")
-dimensions = input(
-    "Entrer les dimensions du meuble sous le format : hauteur;Largeur;Profondeur (en mm) :\n")
-li = list(dimensions.split(";"))
-epaisseurMontant = int(input("Entrer l'épaisseur des montants :\n"))
-epaisseurPetit = int(input("Entrer l'épaisseur des étagères :\n"))
-hauteurSocle = int(input("Entrer la hauteur du socle (0 si pas de socle):\n"))
-epaisseurFond = int(input("Entrer Epaisseur du fond (0 si pas de fond):\n"))
-recouvrement = int(input(
-    "Taper '1' si portes et socle en recouvrement, sinon taper '0' :\n"))
-nbCaisson = int(input("Entrer le nombre de caisson :\n"))
+nomProjet = "Feuille Débit - " + input("Nom du projet :\n")
+hauteurTotale = verif("Hauteur Totale (en mm)", 0, hauteurPxMax)
+largeurTotale = verif("Largeur Totale (en mm)", 0, largeurMeubleMax)
+profondeurTotale = verif("Profondeur Totale (en mm)", 0, largeurPxMax)
+epaisseurMontant = verif(
+    "Epaisseur des montants (en mm)", epaisseurMin, epaisseurMax)
+epaisseurPetit = verif(
+    "Epaisseur des portes et étagères (en mm)", epaisseurMin, epaisseurMax)
+hauteurSocle = verif("Hauteur du socle (0 si pas de socle)", 0, hauteurTotale)
+epaisseurFond = verif("Epaisseur du fond (0 si pas de fond)", 0, epaisseurMax)
+recouvrement = verif(
+    "Taper '1' si portes et socle en recouvrement, sinon taper '0'", 0, 1)
+nbCaisson = verif("Nombre de caisson", 1, 20)
 
 # VARIABLES
-taillePxMax = 1200
-hauteurTotale = int(li[0])
-largeurTotale = int(li[1])
-profondeurTotale = int(li[2])
+fichierFournisseur = cheminDossier + "\\" + nomProjet + '.txt'
+fichierDetail = cheminDossier + "\\" + nomProjet + 'detail.txt'
 if recouvrement != 0:
     profMontant = profondeurTotale - epaisseurPetit
 else:
@@ -42,18 +59,20 @@ largeurInt = (largeurTotale - 2 * epaisseurMontant)
 nbMontant = nbCaisson - 1
 
 # CREATION FICHIERS TXT
-with open(nomProjet + '.txt', 'w') as f:
+with open(fichierFournisseur, 'w') as f:
     f.write("En " + str(epaisseurMontant) + "mm d'épaisseur\n")
     f.close
 
-with open(nomProjet + 'detail.txt', 'w') as fc:
-    fc.write(nomProjet + "\nDimensions : " + str(dimensions) + "mm\n")
+with open(fichierDetail, 'w') as fc:
+    fc.write(nomProjet + "\nh" + str(hauteurTotale) + "xl" +
+             str(largeurTotale) + "xp" + str(profondeurTotale) + "mm\n")
     fc.write("Epaisseur Montant : " + str(epaisseurMontant) +
              " mm ; Etagères : " + str(epaisseurPetit) + "mm\n")
     fc.write("Hauteur du socle : " + str(hauteurSocle) + " mm\nEpaisseur du fond : " +
              str(epaisseurFond) + " mm\nRecouvrement : " + str(recouvrement) + "\n\n")
     fc.close
 
+# ECRITURE TXT
 writeAll(str(2), str(hauteurTotale), str(profMontant), "Montants Exterieurs")
 
 if nbCaisson > 1:
@@ -61,35 +80,40 @@ if nbCaisson > 1:
              str(profondeurInt), "Montants Interieurs")
 
 if epaisseurPetit < epaisseurMontant:
-    with open(nomProjet + '.txt', 'a') as f:
+    with open(fichierFournisseur, 'a') as f:
         f.write("\nEn " + str(epaisseurPetit) + "mm d'épaisseur\n")
         f.close
 
 writeAll(str(1), str(largeurTotale), str(profMontant), "Corniche")
 
 for i in range(nbCaisson):
-    largeurcaisson = int(input("Caisson " + str(i+1) + " - Entrer la largeur (" +
-                         str(largeurInt) + " mm disponible) :\n"))  # reponse < Largeur int
+    if nbCaisson > 1:
+        largeurcaisson = int(input("Caisson " + str(i+1) + " - Entrer la largeur (" +
+                             str(largeurInt) + " mm disponible) :\n"))
+    else:
+        largeurcaisson = largeurInt
     porte = int(input("Caisson " + str(i) +
-                " : Entrer le nombre de porte :\n"))  # reponse : 0,1 ou 2
+                " : Entrer le nombre de porte :\n"))
     nbEtageres = int(input("Entrer le nombre d'étagère du caisson :\n")) + 1
 
-    with open(nomProjet + 'detail.txt', 'a') as fc:
+    with open(cheminDossier + "\\" + nomProjet + 'detail.txt', 'a') as fc:
         fc.write("\nCaisson " + str(i+1) + "\nLargeur : " + str(largeurcaisson) +
                  " mm\nNombre de porte : " + str(porte) + "\nNombre d'étagères : " + str(nbEtageres) + "\n")
         fc.close
 
     if recouvrement != 0:
-        hporte = hauteurTotale - hauteurSocle - 4
-        lporte = int((largeurcaisson / porte) - (porte * 4))
-        writeAll(str(porte), str(hporte), str(lporte), "Portes")
+        if porte != 0:
+            hporte = hauteurTotale - hauteurSocle - 4
+            lporte = int((largeurcaisson / porte) - (porte * 4))
+            writeAll(str(porte), str(hporte), str(lporte), "Portes")
         if hauteurSocle != 0 and i < 2:
             writeAll(str(1), str((largeurTotale)),
                      str(hauteurSocle), "Bandeau Socle")
     else:
-        hporte = hauteurInterieur - 4
-        lporte = int(largeurcaisson / porte - porte * 4)
-        writeAll(str(porte), str(hporte), str(lporte), "Portes")
+        if porte != 0:
+            hporte = hauteurInterieur - 4
+            lporte = int(largeurcaisson / porte - porte * 4)
+            writeAll(str(porte), str(hporte), str(lporte), "Portes")
         if hauteurSocle != 0:
             writeAll(str(1), str((largeurcaisson)),
                      str(hauteurSocle), "Bandeau Socle")
@@ -102,12 +126,12 @@ if epaisseurFond > 0:
     largFond = largeurTotale + 20 - 2 * epaisseurMontant
     hauteurFond = hauteurInterieur + 20
 
-    with open(nomProjet + '.txt', 'a') as f:
+    with open(fichierFournisseur, 'a') as f:
         f.write("\nEn " + str(epaisseurFond) + "mm d'épaisseur\n")
         f.close
-    with open(nomProjet + 'detail.txt', 'a') as fc:
+    with open(fichierDetail, 'a') as fc:
         fc.write("\n\nFond\n")
         fc.close
-    if largFond > taillePxMax:
+    if largFond > largeurPxMax:
         largFond = largFond / 2
     writeAll(str(2), str(hauteurFond), str(largFond), "Fond")
